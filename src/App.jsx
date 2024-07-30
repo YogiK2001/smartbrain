61615eac9bb7351960321c7d575ea1dd9947cad9
YogiK2001 <kaustubh7718@gmail.com>
2024-07-30 19:59:06 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -114,7 +114,7 @@ const onSubmit = () => {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://smartbrain-api-azih.onrender.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
